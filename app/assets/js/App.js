be51d0f85d7170dc498dc1dd0480109e7b8b3852
Lucian <paunalucian14@gmail.com>
2020-01-26 15:06:30 +0000

    Completed sticky header

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,12 +1,14 @@
 import '../css/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 
 new RevealOnScroll(document.querySelectorAll('.feature-item')); 
 new RevealOnScroll(document.querySelectorAll('.testimonial')); 
 
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 
 if (module.hot) {
   module.hot.accept();
